f12f79863e9195517899d629d1e2ab6a7de8488a
Andrew Roberts <adroberts@gmail.com>
2026-04-09 17:29:55 -0400

    Add keymaster formula

---
+++ Formula/keymaster.rb
@@ -0,0 +1,20 @@
+class Keymaster < Formula
+  desc "TouchID-protected keychain access for scripts"
+  homepage "https://github.com/aroberts/keymaster"
+  url "https://github.com/aroberts/keymaster/archive/refs/tags/v0.3.0.tar.gz"
+  sha256 "7c2ca085fd53dddb00a6e343cbf2e932ef2f2632c9a8457671ab23e551ea626e"
+  license "MIT"
+
+  depends_on :macos
+
+  def install
+    system "swiftc", "keymaster.swift", "-o", "keymaster", "-O"
+    bin.install "keymaster"
+    bin.install "bin/keymaster-askpass"
+    bin.install "bin/keymaster-ssh"
+  end
+
+  test do
+    assert_match "keymaster", shell_output("#{bin}/keymaster 2>&1", 1)
+  end
+end
