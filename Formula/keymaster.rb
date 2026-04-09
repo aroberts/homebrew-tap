65ef12e7a92f830076c89e9d56eea72bb48ec88c
Andrew Roberts <adroberts@gmail.com>
2026-04-09 18:24:37 -0400

    Add HEAD build support

--- Formula/keymaster.rb
+++ Formula/keymaster.rb
@@ -4,6 +4,7 @@ class Keymaster < Formula
   url "https://github.com/aroberts/keymaster/archive/refs/tags/v0.3.0.tar.gz"
   sha256 "7c2ca085fd53dddb00a6e343cbf2e932ef2f2632c9a8457671ab23e551ea626e"
   license "MIT"
+  head "https://github.com/aroberts/keymaster.git", branch: "master"
 
   depends_on :macos
 
